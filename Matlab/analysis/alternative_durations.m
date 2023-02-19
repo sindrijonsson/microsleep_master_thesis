645d3f535428db2ab888e5a98839eaf023207265
sindrijonsson <sindrijonsson2@gmail.com>
2023-02-19 13:24:31 -0800

    fixed per event metrics to evaluated depending on is_pos, generated tables and updated requirements

--- Matlab/analysis/alternative_durations.m
+++ Matlab/analysis/alternative_durations.m
@@ -29,20 +29,20 @@ for i = 1:numel(tests)
 
 end
 
-tSamp.models = mdls; 
-tSamp = tSamp(:,[size(tSamp,2),(1:end-1)])
+tSamp.model = mdls; 
+tSamp = tSamp(:,[size(tSamp,2),(1:end-1)]);
 
-tSampPerRec.models = mdls;
-tSampPerRec = tSampPerRec(:,[end,(1:end-1)])
+tSampPerRec.model = mdls;
+tSampPerRec = tSampPerRec(:,[end,(1:end-1)]);
 
-tEvent.models = mdls;
-tEvent = tEvent(:,[size(tEvent,2),(1:end-1)])
+tEvent.model = mdls;
+tEvent = tEvent(:,[size(tEvent,2),(1:end-1)]);
 
-tEventPerRec.models = mdls;
-tEventPerRec = tEventPerRec(:,[size(tEventPerRec,2),(1:end-1)])
+tEventPerRec.model = mdls;
+tEventPerRec = tEventPerRec(:,[size(tEventPerRec,2),(1:end-1)]);
 
-% write_results_table(tSamp, tSampPerRec, "sample", params, "alt");
-% write_results_table(tEvent, tEventPerRec, "event", params, "alt");
+write_results_table(tSamp, tSampPerRec, "sample", params, "alt");
+write_results_table(tEvent, tEventPerRec, "event", params, "alt");
 
 function entry = pretty_per_rec(perRec, by)
     [m, s] = summarize_perRecMetrics(perRec, by);
--- Matlab/utils/calc_hit_stats.m
+++ Matlab/utils/calc_hit_stats.m
@@ -4,25 +4,37 @@ if nargin < 3; hdl=0; end
 
 [tp, fp, fn] = calc_hits_with_singles(yTrue, yHat);
 
-% Precision
-if (tp + fp) > 0
-    precision = tp / (tp + fp);
-else
-    precision = 0;
-end
+% Check if there are any positive classes
+[f,~,s] = get_first_and_last(yTrue, 1, 0);
+is_pos = (length(f) + length(s) ) > 1;
 
-% Recall
-if (tp + fn) > 0
-   recall = tp / (tp + fn);
-else
-   recall = 0;
-end
+if is_pos
+    
+    % Precision
+    if (tp + fp) > 0
+        precision = tp / (tp + fp);
+    else
+        precision = 0;
+    end
+    
+    % Recall
+    if (tp + fn) > 0
+       recall = tp / (tp + fn);
+    else
+       recall = 0;
+    end
+    
+    % F1 score
+    if (precision + recall) > 0
+       f1 = (2 * precision * recall) / (precision + recall);
+    else
+       f1 = 0;
+    end
 
-% F1 score
-if (precision + recall) > 0
-   f1 = (2 * precision * recall) / (precision + recall);
 else
-   f1 = 0;
+    precision = nan;
+    recall = nan;
+    f1 = nan;
 end
 
 stats=struct;
